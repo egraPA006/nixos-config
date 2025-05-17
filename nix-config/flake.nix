3c8cc7e24617e95d1dc5d60b40b6149300ae4da3
Egor Pustovoytenko <puseg2006@gmail.com>
2025-05-17 18:00:18 +0300

    Update

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -14,27 +14,23 @@
     # !!! USER CONFIG: Change this to switch profiles !!!
     selectedSystem = "laptop";
   in {
-    nixosConfigurations = 
-      if builtins.elem selectedSystem supportedSystems then {
-        "${selectedSystem}" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            (./hosts + "/${selectedSystem}.nix")
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useUserPackages = true;
-                useGlobalPkgs = true;
-                users.egrapa = import ./home/home.nix;
-                extraSpecialArgs = { inherit inputs selectedSystem; };
-              };
-            }
-          ];
-          specialArgs = { inherit inputs selectedSystem; };
-        };
-      } else throw ''
-        Unsupported system profile: "${selectedSystem}".
-        Valid options: ${toString supportedSystems}
-      '';
+    nixosConfigurations =  {
+      "${selectedSystem}" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (./hosts + "/${selectedSystem}.nix")
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.egrapa = import ./home/home.nix;
+              extraSpecialArgs = { inherit inputs selectedSystem; };
+            };
+          }
+        ];
+        specialArgs = { inherit inputs selectedSystem; };
+      };
+    };
   };
 }
