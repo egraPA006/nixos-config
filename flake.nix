e937aed4daf20199db889d0d7ebf5720e9b4de3d
Egor Pustovoytenko <puseg2006@gmail.com>
2025-05-20 19:39:56 +0300

    Update

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      virtualbox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (./hosts/virtualbox/configuration.nix)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.egrapa = import ./hosts/virtualbox/home.nix;
+            };
+          }
+        ];
+      };
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (./hosts/laptop/configuration.nix)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              useGlobalPkgs = true;
+              users.egrapa = import ./hosts/laptop/home.nix;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ hosts/virtualbox/home.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  imports = [
+    # ./neovim.nix
+    # ./sway.nix
+    # ./zsh.nix
+  ];
+
+  # Basic Home Manager settings
+  home = {
+    username = "egrapa";  # Replace with your actual username
+    homeDirectory = lib.mkForce "/home/egrapa";
+    stateVersion = "24.11";     # Match your NixOS version
+
+    # Packages that should be installed specifically for the user
+    packages = with pkgs; [
+      # Add any user-specific packages here
+      # (most should be in system.nix or utilities.nix)
+    ];
+  };
+
+  # Enable Home Manager
+  programs.home-manager.enable = true;
+
+  # GTK/Qt theming (optional but recommended for Sway)
+  gtk.enable = true;
+  qt.enable = true;
+
+  # XDG Desktop Portal configuration (required for Wayland)
+  xdg = {
+    enable = true;
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
+
+  # Systemd services for user session
+  # services = {
+  #   # SSH agent (complements your zsh.nix setup)
+  #   ssh-agent.enable = true;
+
+  #   # GPG agent for password management
+  #   gpg-agent = {
+  #     enable = true;
+  #     pinentryFlavor = "gtk2";
+  #     enableSshSupport = true;
+  #   };
+  # };
+
+  # Dotfile management
+  # home.file = {
+  #   # Example: Manage a specific config file
+  #   ".config/example.conf".text = ''
+  #     # Config file content
+  #   '';
+  # };
+
+  # Environment variables
+  # home.sessionVariables = {
+  #   EDITOR = "nvim";
+  #   VISUAL = "nvim";
+  #   TERMINAL = "kitty";
+  #   BROWSER = "firefox";
+  # };
+
+  # Let Home Manager install and manage itself
+}
