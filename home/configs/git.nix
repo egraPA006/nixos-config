bf3be3652eeb92703b3e8c7e0f87470de4ef9d3a
Egor Pustovoytenko <puseg2006@gmail.com>
2025-05-23 13:40:25 +0300

    Working v1.0

--- home/configs/git.nix
+++ home/configs/git.nix
@@ -3,41 +3,42 @@
 {
   programs.git = {
     enable = true;
-    package = pkgs.git;
-
-    # User info (change these)
-    userName = "Egor Pustovoytenko";
-    userEmail = "puseg2006@gmail.com";
-
-    # Core settings
-    extraConfig = {
-      core = {
-        editor = lib.mkDefault "vim";  # Change to your preferred editor
-        pager = "less -FRX";
-        autocrlf = "input";
-      };
-      color = {
-        ui = "auto";
-        diff = "auto";
-        status = "auto";
-      };
-      pull = {
-        rebase = true;
-      };
-      init = {
-        defaultBranch = "main";
-      };
-    };
-
-    # Minimal ignores
-    ignores = [
-      "*~"
-      "*.swp"
-      ".DS_Store"
-      "*.pyc"
-      "__pycache__"
-      "node_modules/"
-      "result"
-    ];
   };
+  #   # package = pkgs.git;
+
+  #   # # User info (change these)
+  #   # userName = "Egor Pustovoytenko";
+  #   # userEmail = "puseg2006@gmail.com";
+
+  #   # # Core settings
+  #   # extraConfig = {
+  #   #   core = {
+  #   #     editor = "vim";  # Change to your preferred editor
+  #   #     pager = "less -FRX";
+  #   #     autocrlf = "input";
+  #   #   };
+  #   #   color = {
+  #   #     ui = "auto";
+  #   #     diff = "auto";
+  #   #     status = "auto";
+  #   #   };
+  #   #   pull = {
+  #   #     rebase = true;
+  #   #   };
+  #   #   init = {
+  #   #     defaultBranch = "main";
+  #   #   };
+  #   # };
+
+  #   # # Minimal ignores
+  #   # ignores = [
+  #   #   "*~"
+  #   #   "*.swp"
+  #   #   ".DS_Store"
+  #   #   "*.pyc"
+  #   #   "__pycache__"
+  #   #   "node_modules/"
+  #   #   "result"
+  #   # ];
+  # };
 }
--- home/configs/vscode.nix
+++ home/configs/vscode.nix
@@ -7,90 +7,91 @@
       # Additional packages to include in the FHS environment
       zlib
     ]);
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # Core functionality
+        vscodevim.vim
+        eamodio.gitlens
+        editorconfig.editorconfig
 
-    extensions = with pkgs.vscode-extensions; [
-      # Core functionality
-      vscodevim.vim
-      eamodio.gitlens
-      editorconfig.editorconfig
+        # C/C++
+        ms-vscode.cpptools
+        ms-vscode.cmake-tools
+        twxs.cmake
 
-      # C/C++
-      ms-vscode.cpptools
-      ms-vscode.cmake-tools
-      twxs.cmake
+        # Rust
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        serayuzgur.crates
 
-      # Rust
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      serayuzgur.crates
+        # Python
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-toolsai.jupyter
+        ms-toolsai.jupyter-keymap
+        ms-toolsai.jupyter-renderers
 
-      # Python
-      ms-python.python
-      ms-python.vscode-pylance
-      ms-toolsai.jupyter
-      ms-toolsai.jupyter-keymap
-      ms-toolsai.jupyter-renderers
+        # Nix
+        jnoortheen.nix-ide
+        arrterian.nix-env-selector
+        bbenoist.nix
 
-      # Nix
-      jnoortheen.nix-ide
-      arrterian.nix-env-selector
-      bbenoist.nix
+        # Other useful extensions
+        usernamehw.errorlens
+        mkhl.direnv
+        yzhang.markdown-all-in-one
+        gruntfuggly.todo-tree
+      ];
 
-      # Other useful extensions
-      usernamehw.errorlens
-      mkhl.direnv
-      yzhang.markdown-all-in-one
-      gruntfuggly.todo-tree
-    ];
+      userSettings = {
+        # Core settings
+        "editor.fontSize" = 14;
+        "editor.fontFamily" = "'FiraCode Nerd Font', 'Droid Sans Mono', 'monospace', monospace";
+        "editor.tabSize" = 2;
+        "editor.renderWhitespace" = "selection";
+        "files.autoSave" = "afterDelay";
+        "workbench.colorTheme" = "Default Dark Modern";
 
-    userSettings = {
-      # Core settings
-      "editor.fontSize" = 14;
-      "editor.fontFamily" = "'FiraCode Nerd Font', 'Droid Sans Mono', 'monospace', monospace";
-      "editor.tabSize" = 2;
-      "editor.renderWhitespace" = "selection";
-      "files.autoSave" = "afterDelay";
-      "workbench.colorTheme" = "Default Dark Modern";
+        # Language specific settings
+        "[nix]".editor.tabSize = 2;
+        "[python]".editor.tabSize = 4;
+        "python.linting.enabled" = true;
+        "python.linting.pylintEnabled" = true;
+        "python.formatting.provider" = "black";
+        "python.analysis.typeCheckingMode" = "basic";
 
-      # Language specific settings
-      "[nix]".editor.tabSize = 2;
-      "[python]".editor.tabSize = 4;
-      "python.linting.enabled" = true;
-      "python.linting.pylintEnabled" = true;
-      "python.formatting.provider" = "black";
-      "python.analysis.typeCheckingMode" = "basic";
+        # Rust settings
+        "rust-analyzer.checkOnSave.command" = "clippy";
+        "rust-analyzer.lens.enable" = false;
+        "rust-analyzer.updates.askBeforeDownload" = false;
 
-      # Rust settings
-      "rust-analyzer.checkOnSave.command" = "clippy";
-      "rust-analyzer.lens.enable" = false;
-      "rust-analyzer.updates.askBeforeDownload" = false;
+        # C/C++ settings
+        "C_Cpp.default.cppStandard" = "c++20";
+        "C_Cpp.default.cStandard" = "c17";
+        "cmake.configureOnOpen" = true;
 
-      # C/C++ settings
-      "C_Cpp.default.cppStandard" = "c++20";
-      "C_Cpp.default.cStandard" = "c17";
-      "cmake.configureOnOpen" = true;
+        # Nix settings
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings".nil.formatting.command = [ "nixpkgs-fmt" ];
 
-      # Nix settings
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings".nil.formatting.command = [ "nixpkgs-fmt" ];
+        # Jupyter settings
+        "jupyter.askForKernelRestart" = false;
+        "jupyter.alwaysTrustNotebooks" = true;
+      };
 
-      # Jupyter settings
-      "jupyter.askForKernelRestart" = false;
-      "jupyter.alwaysTrustNotebooks" = true;
+      keybindings = [
+        {
+          key = "ctrl+shift+r";
+          command = "workbench.action.terminal.runRecentCommand";
+          when = "terminalFocus";
+        }
+        {
+          key = "ctrl+shift+e";
+          command = "workbench.view.explorer";
+        }
+      ];
     };
-
-    keybindings = [
-      {
-        key = "ctrl+shift+r";
-        command = "workbench.action.terminal.runRecentCommand";
-        when = "terminalFocus";
-      }
-      {
-        key = "ctrl+shift+e";
-        command = "workbench.view.explorer";
-      }
-    ];
   };
 
   home.packages = with pkgs; [
@@ -104,5 +105,5 @@
     clang-tools
     jupyter
   ];
-  git.programs.extraConfig.core.editor = "code";
+  programs.git.extraConfig.core.editor = "code";
 }
