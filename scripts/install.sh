56d02488821070dad500142915f803470e57ec78
Egor Pustovoytenko <puseg2006@gmail.com>
2026-08-02 20:16:44 +0300

    Fix bug in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,11 +14,12 @@ if [ ! -d "hosts/$HOST" ]; then
   exit 1
 fi
 
-echo "Copying repo to /mnt/etc/nixos..."
-sudo cp -r "$REPO_DIR" /mnt/etc/nixos
+echo "Copying repo to /mnt/home/egrapa/nixos-config..."
+sudo mkdir -p /mnt/home/egrapa/
+sudo cp -r "$REPO_DIR" /mnt/home/egrapa/nixos-config
 
 echo "Installing NixOS for $HOST..."
-sudo nixos-install --flake "/mnt/etc/nixos#$HOST"
+sudo nixos-install --flake "/mnt/home/egrapa/nixos-config#$HOST"
 
 echo ""
 echo "Done. Reboot and set password with: passwd egrapa"
