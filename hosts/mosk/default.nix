5d5a3524ac056fc77568af7eaebbf1262d01cd24
Egor Pustovoytenko <puseg2006@gmail.com>
2026-08-09 19:06:18 +0300

    add domain

--- hosts/mosk/default.nix
+++ hosts/mosk/default.nix
@@ -15,11 +15,15 @@
     home = "/home/vincent";
   };
   pino.configDir = "${config.pino.user.home}/nixos-config";
-  pino.server.passwordSync.devices.re-1.id =
-    "LVLTAKU-35ENIVC-BSHN3KB-5FMLBUQ-YBXHKAA-IAB6QTT-2NVKQFB-BZHXCQU";
 
   # Public domain, service profiles, and client device IDs are selected before
   # staging. scripts/server-stage.sh replaces the disk placeholder and
   # generates hardware.nix on the installation machine.
   system.stateVersion = "25.05";
+  pino.server = {
+    domain = "egrapa.com";
+    acmeEmail = "admin@egrapa.com";
+
+    passwordSync.devices.re-1.id = "LVLTAKU-35ENIVC-BSHN3KB-5FMLBUQ-YBXHKAA-IAB6QTT-2NVKQFB-BZHXCQU";
+  };
 }
