331ba27b1093debe64d1b2a4c473154e1a711dc8
Egor Pustovoytenko <puseg2006@gmail.com>
2026-08-15 00:34:59 +0300

    re-1: disable hotspot

--- hosts/re-1/active-profiles.nix
+++ hosts/re-1/active-profiles.nix
@@ -1,2 +1,2 @@
 # Managed by pino profile. Do not edit manually.
-[ "codex" "git" "desktop-apps" "desktop-audio" "desktop-bluetooth" "gnome" "vscode" "vpn" "hotspot" "gaming-full" "datasets" "system-monitor" "torrent" ]
+[ "codex" "git" "desktop-apps" "desktop-audio" "desktop-bluetooth" "gnome" "vscode" "vpn" "gaming-full" "datasets" "system-monitor" "torrent" ]
